9dc6c01f41f44e7d9d9c5d43589bf908efbe4e0d
josepbaratechyaba <110386612+josepbaratechyaba@users.noreply.github.com>
2022-10-20 10:25:13 +0200

    Add files via upload

---
+++ myproject.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 17:55:55 2022
+
+@author: josepbaratech
+"""
+
+import streamlit as st
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+st.header('This is a header')
+
+    
